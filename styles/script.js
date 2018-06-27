e52d4355c590da909ebdf79bcc80b25ed717cff3
Khalid Hosein <khalid.hosein9@gmail.com>
2018-06-27 12:23:51 -0400

    Seperated scripts into own file and fixed everything

---
+++ styles/script.js
@@ -0,0 +1,47 @@
+function showProjects() {
+    console.log("show projects")
+    let project = document.getElementById("projects");
+    let about = document.getElementById("about");
+    let image = document.getElementById("me")
+    let contact = document.getElementById("map")
+    let skills = document.getElementById("skills");
+    project.style.display = "block"
+    about.style.display = "none"
+    image.style.display = "none"
+    contact.style.display = "none"
+    skills.style.display = "none"
+
+
+
+};
+
+function showSkills() {
+    let skills = document.getElementById("skills");
+    let about = document.getElementById("about");
+    let image = document.getElementById("me")
+    let project = document.getElementById("projects");
+    let contact = document.getElementById("map");
+
+    project.style.display = "none"
+    about.style.display = "none"
+    image.style.display = "none"
+    contact.style.display = "none"
+    skills.style.display = "block"
+
+
+};
+
+function showLocation() {
+    let map = document.getElementById("map")
+    let skills = document.getElementById("skills");
+    let about = document.getElementById("about");
+    let image = document.getElementById("me")
+    let project = document.getElementById("projects");
+
+    project.style.display = "none"
+    about.style.display = "none"
+    image.style.display = "none"
+    map.style.display = "block"
+    skills.style.display = "none"
+
+};
